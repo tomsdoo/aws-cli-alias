8adac8cd555e17c89d761a958203a3cad45f49a5
tomsdoo <toms@live.jp>
2024-08-11 08:18:53 +0900

    kv

---
+++ install_kv.js
@@ -0,0 +1,23 @@
+const { writeFile, unlink } = require("fs/promises");
+const path = require("path");
+const {
+  awsCliAliasFilePath,
+  downloadCustomJs,
+  readExistingAliasContent,
+  makeNextAliasContent,
+} = require("./util");
+
+const ALIAS_NAME = "kv";
+
+const JS_URL = `https://raw.githubusercontent.com/tomsdoo/aws-cli-alias/HEAD/${ALIAS_NAME}.js`;
+
+(async () => {
+  await downloadCustomJs(JS_URL, ALIAS_NAME);
+  await writeFile(
+    awsCliAliasFilePath,
+    makeNextAliasContent(ALIAS_NAME, await readExistingAliasContent()),
+    { encoding: "utf8" }
+  );
+  await unlink(path.join(path.dirname(process.argv[1]), "util.js"));
+  await unlink(process.argv[1]);
+})();
