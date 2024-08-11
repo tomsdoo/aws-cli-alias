68d27292ddf5c39bf5c146c5184137083a44120e
tomsdoo <toms@live.jp>
2024-08-11 14:06:26 +0900

    alias geta

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 /bin/bash -c "ALIAS_NAME=code; $(curl -fsSL https://raw.githubusercontent.com/tomsdoo/aws-cli-alias/HEAD/install.sh)"
 ```
 
+### geta
+
+``` sh
+/bin/bash -c "ALIAS_NAME=geta; $(curl -fsSL https://raw.githubusercontent.com/tomsdoo/aws-cli-alias/HEAD/install.sh)"
+```
+
 ### kv
 
 ``` sh
---
+++ install_geta.js
@@ -0,0 +1,21 @@
+const { writeFile, unlink } = require("fs/promises");
+const path = require("path");
+const {
+  awsCliAliasFilePath,
+  downloadCustomJs,
+  readExistingAliasContent,
+  makeNextAliasContent,
+} = require("./util");
+
+const ALIAS_NAME = "geta";
+
+(async () => {
+  await downloadCustomJs(ALIAS_NAME);
+  await writeFile(
+    awsCliAliasFilePath,
+    makeNextAliasContent(ALIAS_NAME, await readExistingAliasContent()),
+    { encoding: "utf8" }
+  );
+  await unlink(path.join(path.dirname(process.argv[1]), "util.js"));
+  await unlink(process.argv[1]);
+})();
